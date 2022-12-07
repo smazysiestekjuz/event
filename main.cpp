6bb5a0c33c9cd4fe563af5b983919cf024a9987e
Mateusz Pniewski <smazysiestekjuz@gmail.com>
2022-12-07 12:33:41 +0100

    builders set

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include "event.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+   Event w1("Finding a job as a programmer",1,1,2023,9,15);
+
+   w1.show();
+
+
     return 0;
 }
